5db410e87dd56eb88ed8f61713b8bd796524d93d
MistryVishwa <146713461+MistryVishwa@users.noreply.github.com>
2026-03-14 16:03:23 +0530

    Create webapp.py

---
+++ webapp.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("Iris Prediction")
+
+import pickle
+model = pickle.load(open("model_iris.pkl", "rb"))
+
+sl = st.slider("SL", 2.0, 10.0)
+sw = st.slider("SW", 2.0, 10.0)
+pl = st.slider("PL", 2.0, 10.0)
+pw = st.slider("PW", 2.0, 10.0)
+
+if st.button("Predict"):
+    st.success(model.predict([[sl, sw, pl, pw]]))
